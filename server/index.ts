eb2b097414ea009ea97fe5c7d0bc57ae229142f0
Joakim Myhre Wibe <71314249+JoakimWibe@users.noreply.github.com>
2025-01-16 14:02:40 +0100

    updated index.ts

--- server/index.ts
+++ server/index.ts
@@ -3,29 +3,21 @@ import express from 'express';
 import cors from 'cors';
 import { readdirSync } from 'fs';
 import db from './db/db';
+import path from 'path';
 
-const port = process.env.PORT || 3000;
 const app = express();
 
 // Middlewares
 app.use(express.json());
 app.use(cors());
 
-const server = async () => {
-    try {
-        await db();
-        const routes = readdirSync('./routes');
-        for (const route of routes) {
-            const { default: router } = await import(`./routes/${route}`);
-            app.use('/api', router);
-        }
-        app.listen(port, () => {
-            console.log(`Server is running on port ${port}`);
-        });
-    } catch (error) {
-        console.error('Failed to start server:', error);
-        process.exit(1);
-    }
-}
+// Connect to database
+db().catch(console.error);
 
-server();
+const routePath = path.join(__dirname, 'routes');
+readdirSync(routePath).forEach(async (route) => {
+    const { default: router } = await import(`./routes/${route}`);
+    app.use('/api', router);
+});
+
+export default app;
