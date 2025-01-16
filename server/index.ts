0b13ecf505343da12169a70421821ec055629098
Joakim Myhre Wibe <71314249+JoakimWibe@users.noreply.github.com>
2025-01-16 14:20:18 +0100

    modified vercel.json

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import { readdirSync } from 'fs';
-import db from '../db/db';
+import db from './db/db';
 
 const port = process.env.PORT || 3000;
 const app = express();
