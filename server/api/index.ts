8069a7a27d7a34acbfb0e0b1137b625e3c29b214
Joakim Myhre Wibe <71314249+JoakimWibe@users.noreply.github.com>
2025-01-16 14:07:13 +0100

    created api folder

--- server/api/index.ts
+++ server/api/index.ts
@@ -2,21 +2,21 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import { readdirSync } from 'fs';
-import db from './db/db';
+import db from '../db/db';
 import path from 'path';
 
 const app = express();
 
-// Middlewares
 app.use(express.json());
 app.use(cors());
 
 // Connect to database
 db().catch(console.error);
 
-const routePath = path.join(__dirname, 'routes');
+// Load routes
+const routePath = path.join(__dirname, '../routes');
 readdirSync(routePath).forEach(async (route) => {
-    const { default: router } = await import(`./routes/${route}`);
+    const { default: router } = await import(`../routes/${route}`);
     app.use('/api', router);
 });
 
